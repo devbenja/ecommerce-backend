b4360006488e3adc3c858f171c2d028d3f4178e3
devbenja <cariasbenjamin59@gmail.com>
2024-09-07 05:43:34 -0600

    Add Login Controller

--- controllers/auth/login.controller.js
+++ controllers/auth/login.controller.js
@@ -1,6 +1,6 @@
-import { User } from "../../models/user.model";
+import { User } from "../../models/user.model.js";
 import bcrypt from 'bcryptjs';
-import { createAccesToken } from "../../utils/jwt";
+import { createAccesToken } from "../../utils/jwt.js";
 
 export const login = async (req, res) => {
 
@@ -14,13 +14,17 @@ export const login = async (req, res) => {
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) return res.status(400).json({ message: 'Invalid Credentials' });
 
-        const token = await createAccesToken({ id: user._id, role: user.role, username: user.username });
+        const token = await createAccesToken({ 
+            id: user._id, 
+            role: user.role, 
+            username: user.username 
+        });
 
         res.status(200).json({ token });
 
     } catch (error) {
 
-        res.status(500).json({ error: 'Server error' });
+        res.status(500).json({ error: 'Server Error' });
 
     }
 
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import { register } from "../controllers/auth/register.controller.js";
+import { login } from "../controllers/auth/login.controller.js";
 
 export const authRouter = Router();
 
 authRouter.post('/api/v1/register', register);
+
+authRouter.post('/api/v1/login', login);
